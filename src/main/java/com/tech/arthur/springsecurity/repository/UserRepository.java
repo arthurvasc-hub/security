1ac6ea267918159340d4bdcca52dc35d5b0795fb
arthurvasc <arthurvasc2@gmail.com>
2024-12-10 18:03:36 -0300

    First commit

---
+++ src/main/java/com/tech/arthur/springsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.tech.arthur.springsecurity.repository;
+
+import com.tech.arthur.springsecurity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+}
