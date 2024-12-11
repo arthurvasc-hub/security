4b0b207063dd438fabb0c4dec7a41c4ccdafd4ca
arthurvasc <arthurvasc2@gmail.com>
2024-12-11 18:46:30 -0300

    Added security and authentication settings

---
+++ src/main/java/com/tech/arthur/springsecurity/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.tech.arthur.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+
+}
