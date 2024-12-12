3560f3182139a0cd2e026e9ee52a21d10831be5d
arthurvasc <arthurvasc2@gmail.com>
2024-12-12 18:16:43 -0300

    Adjusting configuration of the AuthenticationController and SecurityConfig.

--- src/main/java/com/tech/arthur/springsecurity/controller/AuthenticationController.java
+++ src/main/java/com/tech/arthur/springsecurity/controller/AuthenticationController.java
@@ -1,9 +1,31 @@
 package com.tech.arthur.springsecurity.controller;
 
+import com.tech.arthur.springsecurity.model.users.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var tokenUsernamePassword = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+        var auth = authenticationManager.authenticate(tokenUsernamePassword);
+        return ResponseEntity.ok(tokenUsernamePassword);
+    }
 
 
 }
---
+++ src/main/java/com/tech/arthur/springsecurity/model/users/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.tech.arthur.springsecurity.model.users;
+
+public record AuthenticationDTO(String username, String password) {
+}
